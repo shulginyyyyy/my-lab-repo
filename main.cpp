5d4b2d378dcc7f2046b2891c917e79f93d5a1ddb
igoryan <bigpeople228@gmail.com>
2024-12-26 00:27:39 +0300

    Добавил код для вычисления разности чисел

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
-#include "sdt.h"
 #include <iostream>
+using namespace std;
 
 int main() {
     int a, b;
-    std::cout << "Введите два числа: ";
-    std::cin >> a >> b;
-    std::cout << "Сумма введенных чисел: " << (a + b) << std::endl;
+    
+    // Ввод чисел
+    cout << "Введите первое число: ";
+    cin >> a;
+    cout << "Введите второе число: ";
+    cin >> b;
+
+    // Вычисление разности
+    int difference = a - b;
+
+    // Вывод разности
+    cout << "Разность введённых чисел: " << difference << endl;
+
     return 0;
 }
