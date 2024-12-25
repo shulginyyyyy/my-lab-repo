6ddb55e19bd79c0d8da779ad5609df6cc1e5e205
igoryan <bigpeople228@gmail.com>
2024-12-26 01:44:26 +0300

    "Added division operation"

--- main.cpp
+++ main.cpp
@@ -3,14 +3,10 @@ using namespace std;
 
 int main() {
     int a, b;
-
-    // Ввод чисел
     cout << "Enter A and B: ";
     cin >> a >> b;
 
-    // Сумма и разность
-    cout << "A + B = " << (a + b) << '\n';
-    cout << "A - B = " << (a - b) << '\n';
+    cout << "A / B: " << (a / b) << '\n';
 
     return 0;
 }
